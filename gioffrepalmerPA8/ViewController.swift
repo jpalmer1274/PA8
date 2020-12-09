7af90f36d52c225e3cc0f0368803a19626183483
Ian Gioffre <iangioffre@gmail.com>
2020-12-09 14:25:12 -0800

    Initial Commit

---
+++ gioffrepalmerPA8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gioffrepalmerPA8
+//
+//  Created by Gioffre, Ian Thomas on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
