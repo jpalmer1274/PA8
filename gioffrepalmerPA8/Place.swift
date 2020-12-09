f24e22af2548079a4f2ccf2f9b3549950f32bfcd
Ian Gioffre <iangioffre@gmail.com>
2020-12-09 14:43:00 -0800

    added place class, don't know for sure if it has the correct typings

---
+++ gioffrepalmerPA8/Place.swift
@@ -0,0 +1,24 @@
+//
+//  Place.swift
+//  gioffrepalmerPA8
+//
+//  Created by Gioffre, Ian Thomas on 12/9/20.
+//
+
+import Foundation
+
+class Place {
+    var id: String
+    var name: String
+    var vicinity: String
+    var rating: String
+    var photoReference: String
+    
+    func Place(id: String, name: String, vicinity: String, rating: String, photoReference: String) {
+        self.id = id
+        self.name = name
+        self.vicinity = vicinity
+        self.rating = rating
+        self.photoReference = photoReference
+    }
+}
