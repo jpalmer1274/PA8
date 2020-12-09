58d538fc63c47099e24c45f5c3ffb25799e93b2a
Ian Gioffre <iangioffre@gmail.com>
2020-12-09 14:55:12 -0800

    added place detail view controller

---
+++ gioffrepalmerPA8/PlaceDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceDetailViewController.swift
+//  gioffrepalmerPA8
+//
+//  Created by Gioffre, Ian Thomas on 12/9/20.
+//
+
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- gioffrepalmerPA8/PlaceTableViewController.swift
+++ gioffrepalmerPA8/PlaceTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlaceTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
