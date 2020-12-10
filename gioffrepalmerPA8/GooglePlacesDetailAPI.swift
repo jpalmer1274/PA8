790b140b58741cff7d3e912c1402c4ddd6e36f31
Ian Gioffre <iangioffre@gmail.com>
2020-12-10 11:21:30 -0800

    comments, formatting, fixed photo as? String warning

--- gioffrepalmerPA8/GooglePlacesDetailAPI.swift
+++ gioffrepalmerPA8/GooglePlacesDetailAPI.swift
@@ -2,7 +2,7 @@
 //  GooglePlacesDetailAPI.swift
 //  gioffrepalmerPA8
 //
-//  Created by John Palmer on 12/9/20.
+//  Created by Gioffre, Ian Thomas and John Palmer on 12/9/20.
 //
 
 import Foundation
@@ -38,39 +38,15 @@ struct GooglePlacesDetailAPI {
     static func fetchDetailResults(placeID: String, completion: @escaping (Place?) -> Void) {
         let url = GooglePlacesDetailAPI.googlePlacesDetailURL(placeID: placeID)
         
-        // we need to make a request to the Flickr API server using the url
-        // we will get back a JSON response (if all goes well)
-        // we will make the request on a background thread using a data task
         let task = URLSession.shared.dataTask(with: url) { (dataOptional, urlResponseOptional, errorOptional) in
-            // this closure executes later...
-            // when the task has received a response from the server
-            // we want to grab the Data object that represents the JSON response (if there is one)
             if let data = dataOptional, let dataString = String(data: data, encoding: .utf8) {
                 print("we got data!!!")
                 print(dataString)
                 
-                // we want to convert the Data to a JSON object, then the JSON object to a Swift dictionary so we can parse the photos from the array
-                // to create [InterestingPhoto]
-                // write a method to do this
                 if let place = place(fromData: data) {
-                    // our goal is to get this array back to ViewController for displaying in the views
-                    // PROBLEMS!!
-                    // MARK: - Threads
-                    // so far, our code in ViewController (for example) has run on the main UI thread
-                    // the main UI thread listens for interactions with views from the user, it calls callbacks in view controllers and delgates, etc.
-                    // we don't want to run long running tasks/code on the main UI thread, why?
-                    // by default, URLSession dataTasks run on a background thread
-                    // that means that this closure we are in right now... runs asynchronously on a background thread
-                    // fetchInterestingPhotos() starts the task (that runs in the background), then it immediately returns (we can't return the array then!!)
-                    
-                    // we need a completion handler (AKA closure) to call code in ViewController when we have a result (success or failure)
-                    // we want to call the completion closure so it runs on the main UI thread and can update our views
-                    // a thread in iOS is managed by a queue
                     DispatchQueue.main.async {
                         completion(place)
                     }
-                    
-                    // should call completion on failure as well
                 }
                 else {
                     DispatchQueue.main.async {
@@ -87,33 +63,24 @@ struct GooglePlacesDetailAPI {
                 }
             }
         }
-        // by default tasks are created in the suspended state
-        // call resume() to start the task
         task.resume()
     }
     
     static func place(fromData data: Data) -> Place? {
-        // if anything goes wrong, return nil
-        
-        // MARK: - JSON
-        // javascript object notation
-        // json is the most commonly used format for passing data around the web
-        // it is really just a dictionary
-        // keys are strings
-        // values are strings, arrays, nested JSON objects, ints, bools, etc.
-        // our goal is to convert the data into a swift dictionary [String: Any]
-        // libaries (like swiftyJSON) that make this really easy
-        // we are gonna do it the long way!
         do {
             let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
             guard let jsonDictionary = jsonObject as? [String: Any], let placeDetails = jsonDictionary["result"] as? [String: Any],
                   let id = placeDetails["place_id"] as? String, let name = placeDetails["name"] as? String, let vicinity = placeDetails["vicinity"] as? String,
                   let rating = placeDetails["rating"] as? Double, let photos = placeDetails["photos"] as? [[String: Any]],
-                  let photo = photos[photos.startIndex] as? [String: Any], let photoReference = photo["photo_reference"] as? String,
                   let openingHours = placeDetails["opening_hours"] as? [String: Any], let openNow = openingHours["open_now"] as? Bool,
                   let phoneNumber = placeDetails["formatted_phone_number"] as? String, let address = placeDetails["formatted_address"] as? String
             else {
-                print("Error parsing JSON")
+                print("Not all data present")
+                return nil
+            }
+            let photo = photos[photos.startIndex]
+            guard let photoReference = photo["photo_reference"] as? String else {
+                print("Not all data present")
                 return nil
             }
             
@@ -133,8 +100,6 @@ struct GooglePlacesDetailAPI {
         
         let task = URLSession.shared.dataTask(with: url) { (dataOptional, urlResponseOptional, errorOptional) in
             if let data = dataOptional, let image = UIImage(data: data) {
-                // task: call completion correctly
-                // update the view to show the image
                 DispatchQueue.main.async {
                     completion(image)
                 }
--- gioffrepalmerPA8/GooglePlacesSearchAPI.swift
+++ gioffrepalmerPA8/GooglePlacesSearchAPI.swift
@@ -2,7 +2,7 @@
 //  GooglePlacesAPI.swift
 //  gioffrepalmerPA8
 //
-//  Created by John Palmer on 12/9/20.
+//  Created by Gioffre, Ian Thomas and John Palmer on 12/9/20.
 //
 
 import Foundation
@@ -39,40 +39,17 @@ struct GooglePlacesSearchAPI {
     static func fetchSearchResults(latitude: String, longitude: String, keyword: String, completion: @escaping ([Place]?) -> Void) {
         let url = GooglePlacesSearchAPI.googlePlacesSearchURL(latitude: latitude, longitude: longitude, keyword: keyword)
         
-        // we need to make a request to the Flickr API server using the url
-        // we will get back a JSON response (if all goes well)
-        // we will make the request on a background thread using a data task
         let task = URLSession.shared.dataTask(with: url) { (dataOptional, urlResponseOptional, errorOptional) in
-            // this closure executes later...
-            // when the task has received a response from the server
-            // we want to grab the Data object that represents the JSON response (if there is one)
             if let data = dataOptional, let dataString = String(data: data, encoding: .utf8) {
                 print("we got data!!!")
                 print(dataString)
                 
-                // we want to convert the Data to a JSON object, then the JSON object to a Swift dictionary so we can parse the photos from the array
-                // to create [InterestingPhoto]
-                // write a method to do this
                 if let places = places(fromData: data) {
                     print("we got an [Place] with \(places.count) photos")
-                    // our goal is to get this array back to ViewController for displaying in the views
-                    // PROBLEMS!!
-                    // MARK: - Threads
-                    // so far, our code in ViewController (for example) has run on the main UI thread
-                    // the main UI thread listens for interactions with views from the user, it calls callbacks in view controllers and delgates, etc.
-                    // we don't want to run long running tasks/code on the main UI thread, why?
-                    // by default, URLSession dataTasks run on a background thread
-                    // that means that this closure we are in right now... runs asynchronously on a background thread
-                    // fetchInterestingPhotos() starts the task (that runs in the background), then it immediately returns (we can't return the array then!!)
                     
-                    // we need a completion handler (AKA closure) to call code in ViewController when we have a result (success or failure)
-                    // we want to call the completion closure so it runs on the main UI thread and can update our views
-                    // a thread in iOS is managed by a queue
                     DispatchQueue.main.async {
                         completion(places)
                     }
-                    
-                    // should call completion on failure as well
                 }
                 else {
                     DispatchQueue.main.async {
@@ -89,41 +66,26 @@ struct GooglePlacesSearchAPI {
                 }
             }
         }
-        // by default tasks are created in the suspended state
-        // call resume() to start the task
         task.resume()
     }
     
     static func places(fromData data: Data) -> [Place]? {
-        // if anything goes wrong, return nil
-        
-        // MARK: - JSON
-        // javascript object notation
-        // json is the most commonly used format for passing data around the web
-        // it is really just a dictionary
-        // keys are strings
-        // values are strings, arrays, nested JSON objects, ints, bools, etc.
-        // our goal is to convert the data into a swift dictionary [String: Any]
-        // libaries (like swiftyJSON) that make this really easy
-        // we are gonna do it the long way!
         do {
             let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
             guard let jsonDictionary = jsonObject as? [String: Any], let placeArray = jsonDictionary["results"] as? [[String: Any]] else {
                 print("Error parsing JSON")
                 return nil
             }
-            // we sucessfully go the array of photos
+            
             print("successfully got placeArray")
+            
             var places = [Place]()
             for placeObject in placeArray {
-                // parse a InterestingPhoto from photoObject ([String: Any])
-                // write a method to do this
                 if let place = place(fromJSON: placeObject) {
                     places.append(place)
                 }
             }
-            // return the array if it its not empty
-            // GS: finished after class
+            
             if !places.isEmpty {
                 return places
             }
@@ -136,14 +98,16 @@ struct GooglePlacesSearchAPI {
     }
     
     static func place(fromJSON json: [String: Any]) -> Place? {
-        // task: finish this method
-        // we need the id, title, datetaken, url_h
-        // GS: finished after class
         guard let id = json["place_id"] as? String, let name = json["name"] as? String, let vicinity = json["vicinity"] as? String,
-              let photos = json["photos"] as? [[String: Any]], let photo = photos[photos.startIndex] as? [String: Any], let photoReference = photo["photo_reference"] as? String,
-              let rating = json["rating"] as? Double, let openingHours = json["opening_hours"] as? [String: Any], let openNow = openingHours["open_now"] as? Bool
+              let photos = json["photos"] as? [[String: Any]], let rating = json["rating"] as? Double,
+              let openingHours = json["opening_hours"] as? [String: Any], let openNow = openingHours["open_now"] as? Bool
         else {
-            print("error, not all info was present")
+            print("Not all data present")
+            return nil
+        }
+        let photo = photos[photos.startIndex]
+        guard let photoReference = photo["photo_reference"] as? String else {
+            print("Not all data present")
             return nil
         }
         
@@ -155,8 +119,6 @@ struct GooglePlacesSearchAPI {
         
         let task = URLSession.shared.dataTask(with: url) { (dataOptional, urlResponseOptional, errorOptional) in
             if let data = dataOptional, let image = UIImage(data: data) {
-                // task: call completion correctly
-                // update the view to show the image
                 DispatchQueue.main.async {
                     completion(image)
                 }
